8a3c6161a690e3567fa9f80727c46b7e8c395d06
pydevdse <pydev.dse@gmail.com>
2023-07-27 17:30:39 -0400

    get_bagzhks

--- bagazhnik/bagazhnik/spiders/spider_bagazhnik.py
+++ bagazhnik/bagazhnik/spiders/spider_bagazhnik.py
@@ -25,7 +25,7 @@ class SpiderBagazhnik(scrapy.Spider):
             url_model = url.xpath('.//a/@href').extract_first()
             if url_model == response.url: continue
             if response.url.split("/")[-1].split(".")[0] in url_model:
-                model = {"brand":brand, "model": model_name, "url": url_model}
+                model = {"brand":brand, "model": model_name}
                 models.append(model)
                 yield scrapy.Request(url_model, callback=self.parse_mosel_mod, cb_kwargs={"model":model})
 
@@ -41,6 +41,26 @@ class SpiderBagazhnik(scrapy.Spider):
             # print(model_mod_name, url_model_mod)
             if url_model_mod == response.url: continue
             if response.url.split("/")[-1].split(".")[0] in url_model_mod:
-                model.update({"model_mod": model_mod_name, "url_mod": url_model_mod})
+                model.update({"model_mod": model_mod_name})
                 model_mods.append(model)
-                yield model
+                yield scrapy.Request(url_model_mod, callback=self.parse_bagazhnik, cb_kwargs={"model":model})
+
+    def parse_bagazhnik(self, response, model):
+        bgzhnks = response.xpath('//div[@class="catalog-section portal-level section"]')
+        active = bgzhnks[0].xpath('.//div[@class="product-item"]/div[@class="product-img"]')
+        disable = bgzhnks[0].xpath('.//div[@class="product-item disabled"]/div[@class="product-img"]')
+        logging.info(f"Mod: {model.get('model_mod')} active count: {len(active)}")
+        logging.info(f"Mod: {model.get('model_mod')} disable count: {len(disable)}")
+        for act in active:
+            mod = model.copy()
+            bagzhnk_url = act.xpath('.//a/@href').extract_first()
+            bagzhnk_name = act.xpath('.//a/img/@alt').extract_first()
+            mod.update({"bagzhnk_name":bagzhnk_name, "bagzhnk_url":bagzhnk_url, "status":"active"})
+            yield mod
+
+        for dis in disable:
+            mod = model.copy()
+            bagzhnk_url = dis.xpath('.//a/@href').extract_first()
+            bagzhnk_name = dis.xpath('.//a/img/@alt').extract_first()
+            mod.update({"bagzhnk_name": bagzhnk_name, "bagzhnk_url": bagzhnk_url, "status": "disabled"})
+            yield mod
