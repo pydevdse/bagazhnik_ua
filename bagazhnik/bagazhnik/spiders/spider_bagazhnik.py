173dae956a875f37e86959e5f9a022a4211ad7a3
pydevdse <pydev.dse@gmail.com>
2023-07-27 08:30:08 -0400

    models

--- bagazhnik/bagazhnik/spiders/spider_bagazhnik.py
+++ bagazhnik/bagazhnik/spiders/spider_bagazhnik.py
@@ -1,3 +1,5 @@
+import logging
+
 import scrapy
 
 
@@ -9,13 +11,23 @@ class SpiderBagazhnik(scrapy.Spider):
         brands_urls = response.xpath('//li[@class="trunk-selection-item"]/a/@href').extract()
         brands_names = response.xpath('//span[@class="trunk-selection-name"]/text()').extract()
         # print(brands_urls)
-        print(len(brands_urls))
-        print(brands_names)
-        print(len(brands_names))
+        print(f"Urls brands count: {len(brands_urls)}")
+        #print(brands_names)
+        print(f"Names brands count: {len(brands_names)}")
         for ind, brand_url in enumerate(brands_urls):
             yield scrapy.Request(brand_url, callback=self.pares_models, cb_kwargs={'brand': brands_names[ind]})
 
     def pares_models(self, response, brand):
-        models = response.xpath('//li[@class="trunk-selection-item"]/a/@href').extract()
-        print(f"Brand: {brand}  models: {models}")
-        print(f"Brand {brand} models count: {len(models)}")
+        urls = response.xpath('//li[@class="trunk-selection-item"]') #.extract()
+        models = []
+        for url in urls:
+            model_name = url.xpath('.//span[@class="trunk-selection-name"]/text()').extract_first()
+            url_model = url.xpath('.//a/@href').extract_first()
+            if response.url.split("/")[-1].split(".")[0] in url_model:
+                models.append({"brand":brand, "model": model_name, "url": url_model})
+
+        if len(models)>1:
+            models=models[1:]
+        #print(f"Brand: {brand}  models: {models}")
+        yield {brand:models}
+        # for model in models_names:
--- main_bagazhnik.py
+++ main_bagazhnik.py
@@ -1,17 +1,22 @@
 import requests
 from lxml import html
+from get_headers import get_headers
 
 def main():
-    url = "https://bagazhnik.ua/bagazhnik-na-kryshu.html"
+    # url = "https://bagazhnik.ua/bagazhnik-na-kryshu.html"
+    url = "https://bagazhnik.ua/bagazhnik-na-kryshu/alfa-romeo/156.html"
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
+    # headers = get_headers("headers_acura.txt")
     response = requests.get(url, headers)
     print(response)
+#    print(response.text)
 
     tree = html.fromstring(response.content)
-
-    brands = tree.xpath('//li[@class="trunk-selection-item"]/a/@href')
-    print(brands)
-    print(len(brands))
+    # a class="trunk-selection-link" href=
+    mods = tree.xpath('//li[@class="trunk-selection-item"]/a/@href')
+    mods = [model for model in mods if "156" in model]
+    print(mods)
+    print(len(mods))
 
 
 if __name__ == '__main__':
